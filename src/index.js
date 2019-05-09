32a7fed62d2ce3cf144eff96139bb687c7b2480f
deathbykender <45900750+abferris@users.noreply.github.com>
2019-05-08 22:18:46 -0700

    lab38done
    done

--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import LoginContext from './components/auth/context.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import LoginContext from "./components/auth/context.js";
+import ToDo from "./components/todo/todo.js";
+import Header from "./components/header/header.js";
 
-import ToDo from './components/todo/todo.js';
-import Header from './components/header/header.js';
-
-import './site.scss';
+import "./site.scss";
 
 function App() {
   return (
-    <BrowserRouter>
-      <LoginContext>
-        <Header />
-        <ToDo />
-      </LoginContext>
-    </BrowserRouter>
+    <LoginContext>
+      <Header />
+      <ToDo />
+    </LoginContext>
   );
 }
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
